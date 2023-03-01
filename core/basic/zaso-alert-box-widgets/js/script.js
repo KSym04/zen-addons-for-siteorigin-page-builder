e27c024b5bbca250336563ec1f07848b721784ce
ksym04 <dev@dopethemes.com>
2023-03-02 05:23:21 +0800

    Remove dependency of Alert widget to jQuery.

--- core/basic/zaso-alert-box-widgets/js/script.js
+++ core/basic/zaso-alert-box-widgets/js/script.js
@@ -1,11 +1,9 @@
 /* [ZASO] Alert Box Template - Main JS */
 
-(function ($) {
-
-    var closeBtn = jQuery('.zaso-alert-box__closebtn');
-    closeBtn.on('click', function(event) {
-        event.preventDefault();
-        jQuery(this).parents('.zaso-alert-box').fadeOut();
-    });
-
-})(jQuery);
+document.addEventListener("DOMContentLoaded", function () {
+	var closeBtn = document.querySelector(".zaso-alert-box__closebtn");
+	closeBtn.addEventListener("click", function (event) {
+		event.preventDefault();
+		this.closest(".zaso-alert-box").style.display = "none";
+	});
+});
